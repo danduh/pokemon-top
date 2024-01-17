0c9373624950e8c1e6a785ec8cc3c8fe2a8e501d
Daniel Ostrovsky <daniel.ostrovsky@dell>
2024-01-17 13:28:12 +0200

    d

--- pokemon/src/environments/environment.prod.ts
+++ pokemon/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const env = {
   isProd: true,
   mfesOrigins: {
-    details: "/assets/details/remoteEntry.js"
-  }
+    details: "/assets/details/remoteEntry.js",
+  },
+  manifestPath:'/pokemon-top/assets/module-federation.manifest.json'
 }
--- pokemon/src/environments/environment.ts
+++ pokemon/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const env = {
   isProd: false,
   mfesOrigins: {
     details:"http://localhost:4202/remoteEntry.js"
-  }
+  },
+  manifestPath:'/assets/module-federation.manifest.json'
 }
