6f67c588652de461a0b439a2753e37a7b357fe6f
shelly_goldblit <shelly_goldblit@dell.com>
2024-02-03 06:55:30 +0200

    move list

--- apps/list/jest.config.ts
+++ apps/list/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'list',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/list',
+  coverageDirectory: '../../coverage/apps/list',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
