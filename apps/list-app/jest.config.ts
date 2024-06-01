abd739f559e7f4007116ac5f5af6f76e64ac6446
shelly_goldblit <shelly_goldblit@dell.com>
2024-06-01 06:28:03 +0300

    rename apps

--- apps/list-app/jest.config.ts
+++ apps/list-app/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'list',
+  displayName: 'list-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/list',
+  coverageDirectory: '../../coverage/apps/list-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
