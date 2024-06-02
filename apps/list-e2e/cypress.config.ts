01082d18c509657e173416c688969a573365ccf2
shelly_goldblit <shelly_goldblit@dell.com>
2024-06-02 15:38:37 +0300

    fix list e2e cypress config

--- apps/list-e2e/cypress.config.ts
+++ apps/list-e2e/cypress.config.ts
@@ -7,4 +7,18 @@ export default defineConfig({
     ...nxE2EPreset(__filename, { cypressDir: 'src' }),
     baseUrl: 'http://localhost:4200',
   },
+  viewportHeight: 960,
+  viewportWidth: 1536,
+  reporter: '../../node_modules/mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results/json',
+    overwrite: false,
+    html: false,
+    json: true,
+    suiteTitleSeparatedBy: ' > ',
+    testCaseSwitchClassnameAndName: false,
+    rootSuiteTitle: 'List E2E Tests',
+    toConsole: true,
+    defaultCommandTimeout: 50000,
+  },
 });
