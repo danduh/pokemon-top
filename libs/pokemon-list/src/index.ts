2a60ab25c0ee05748797c853beaf6997de475a86
shelly_goldblit <shelly_goldblit@dell.com>
2024-06-11 19:54:39 +0300

    fix build

--- libs/pokemon-list/src/index.ts
+++ libs/pokemon-list/src/index.ts
@@ -1,6 +1,4 @@
-export * from './lib/pokemon-list/pokemon-list.component';
-// export * from './lib/pokemon-list/components/header/header.component.test.driver';
 export * from './lib/pokemon-list/components/header/header.component';
-// export * from './lib/pokemon-list/pokemon-list.component.test.driver';
+export * from './lib/pokemon-list/pokemon-list.component';
 export * from './lib/pokemon-list/search/search.component';
 export * from './lib/pokemon-list/services/pokemon.service';
