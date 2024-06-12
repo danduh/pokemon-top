5922512c4fb497c78508d3d4388b2943e2deddb8
shelly_goldblit <shelly_goldblit@dell.com>
2024-06-12 20:39:37 +0300

    add e2e workflow

--- apps/pokemon-e2e/cypress.config.ts
+++ apps/pokemon-e2e/cypress.config.ts
@@ -7,4 +7,18 @@ export default defineConfig({
     ...nxE2EPreset(__filename, { cypressDir: 'src' }),
     baseUrl: 'https://danduh.github.io/pokemon-top',
   },
+  viewportHeight: 960,
+  viewportWidth: 1536,
+  reporter: '../../node_modules/mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results/json',
+    overwrite: false,
+    html: false,
+    json: true,
+    suiteTitleSeparatedBy: ' > ',
+    testCaseSwitchClassnameAndName: false,
+    rootSuiteTitle: 'Pokemon E2E Tests',
+    toConsole: true,
+    defaultCommandTimeout: 50000,
+  },
 });
